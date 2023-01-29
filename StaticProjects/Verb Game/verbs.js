a458d70136ad92c8afa0bfa55215e1b74e469e6d
James <pawson1988@me.com>
2023-01-29 19:23:12 +0100

    added comments

--- StaticProjects/Verb Game/verbs.js
+++ StaticProjects/Verb Game/verbs.js
@@ -2,23 +2,28 @@ import { verbsNotRandom } from "./verbsData.js"
 
 
 
+// add verbsNotRandom.participle to each verbsNotrandom.alternative property that is blank to avoid errors. 
 for(let i = 0; i < verbsNotRandom.length; i++){
     if(verbsNotRandom[i].alternative === ""){
         verbsNotRandom[i].alternative = verbsNotRandom[i].participle;
     }
 }
 
+// initalise a variable to capture user input for how many words they want. 
 let noOfWords = "";
 
+// ask user how many words and ask again if it's not within the number of words in the array
 do {
     noOfWords = prompt("How many words do you want to show?: (Max verbs is: " + verbsNotRandom.length + ") ");
 } while (noOfWords > verbsNotRandom.length);
 
-
+// shuffle array and then slice the array at how many words the user wants, thus only showing how many words the user wants
 const verbs = shuffleArray(verbsNotRandom).slice(0, noOfWords);
 
+// get container from DOM to append children to it 
 const container = document.querySelector(".container");
 
+// create all the lements with the correct attributes for the user's words 
 for(let i = 0; i < verbs.length; i++){
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "word-container");
@@ -38,7 +43,7 @@ for(let i = 0; i < verbs.length; i++){
     newDiv.appendChild(word3Input);
 }
 
-
+//  get the elements from the DOM that were added in the above "function"
 const wordContainers = document.querySelectorAll(".word-container");
 const word1 = document.querySelectorAll(".word1");
 const word2 = document.querySelectorAll(".word2");
@@ -47,7 +52,7 @@ const score2 = document.querySelector(".score2");
 const score3 = document.querySelector(".score3");
 const totalVerbs = document.querySelector(".no-verbs");
 
-
+// initialise variable to be used later 
 totalVerbs.textContent = "Total Verbs: " + verbs.length;
 let word2Score = 0;
 let word3Score = 0;
@@ -55,6 +60,7 @@ let word3Score = 0;
 let word2ScoreTracker = [];
 let word3ScoreTracker = [];
 
+// EventListener for past verbs
     for(let i = 0; i < verbs.length; i++){ 
         word2[i].addEventListener("input", () => {
             if(word2[i].value.toLowerCase() === verbs[i].past.toLowerCase() || word2[i].value.toLowerCase() === verbs[i].alternative.toLowerCase()){
@@ -86,6 +92,7 @@ let word3ScoreTracker = [];
         }
     )}
 
+// EventListener for past participle verbs
     for(let i = 0; i < verbs.length; i++){ 
         word3[i].addEventListener("input", () => {
             if(word3[i].value.toLowerCase() === verbs[i].participle.toLowerCase() || word3[i].value.toLowerCase() === verbs[i].alternative.toLowerCase()){
@@ -107,7 +114,7 @@ let word3ScoreTracker = [];
                     alert("You got all the past participle verbs!! congrats!!");
                 }
                 
-                if(verbs[i].participleDone === true && verbs[i].pastDone === true){
+                if(verbs[i].participleDone && verbs[i].pastDone){
                     word3[i].parentElement.style.backgroundColor = "#a9e7b3";
                 }
             } else {
@@ -120,6 +127,7 @@ for(let i = 0; i < verbs.length; i++){
         word1[i].textContent = verbs[i].present.toUpperCase();
 }
 
+// shuffle array function
 function shuffleArray(array){
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
